da9aaf3f46245633d6948d8adeb26371f416a020
AyushNautiyalDeveloper <ayush@betalectic.com>
2024-04-08 01:17:20 +0530

    added the required chages for publishing the extension.

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,32 @@
 import * as vscode from "vscode";
+import axios from "axios";
+import { XMLParser } from "fast-xml-parser";
+export async function activate(context: vscode.ExtensionContext) {
+  const response = await axios.get("https://blog.webdevsimplified.com/rss.xml");
+  //   console.log(response);
+  const articles = new XMLParser()
+    .parse(response.data)
+    .rss.channel.item.map((article: any) => {
+      return {
+        label: article.title,
+        description: article.description,
+        link: article.link,
+      };
+    });
 
-export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "manthan" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "saveNotes-command",
+    async () => {
+      const article: any = await vscode.window.showQuickPick(articles, {
+        matchOnDetail: true,
+      });
 
-  let disposable = vscode.commands.registerCommand("manthan.helloWorld", () => {
-    vscode.window.showInformationMessage("Hello World from manthan!");
-  });
+      if (!article) return;
+      vscode.env.openExternal(article.link);
+      console.log(article);
+      //   vscode.window.showInformationMessage("saving the notes");
+    }
+  );
 
   context.subscriptions.push(disposable);
 }
